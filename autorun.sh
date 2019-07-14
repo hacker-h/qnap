1388e0e5663a5cd1621ef8e707f2e149c6ce3b60
Henning Häcker <henning.haecker@web.de>
2019-07-14 04:33:04 +0200

    fix path

--- autorun.sh
+++ autorun.sh
@@ -10,7 +10,7 @@ rm -rf /usr/sbin/noip2
 rm -rf /usr/sbin/noip2c
 
 #add autorun if necessarry
-grep autorun /etc/config/qpkg.conf || \
+grep autorun /etc/config/qpkg.conf > /dev/null || \
 (
 echo "
 
@@ -18,13 +18,13 @@ echo "
 Name = autorun
 Version = 1.0
 QPKG_File = autorun.qpkg // Ohne diese Zeile wird das Script deaktiviert!
-Shell = /share/CACHEDEV1_DATA/.qpkg/autorun/autorun.sh // ggf. anpassen
-Install_Path = /share/CACHEDEV1_DATA/.qpkg/autorun // ggf. anpassen
+Shell = /share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh // ggf. anpassen
+Install_Path = /share/CE_CACHEDEV1_DATA/.qpkg/autorun // ggf. anpassen
 Enable = TRUE
 Official = 0
 " >> /etc/config/qpkg.conf && \
-rm -rf /share/CACHEDEV1_DATA/.qpkg/autorun && \
-mkdir -p /share/CACHEDEV1_DATA/.qpkg/autorun && \
-cp "${SCRIPTPATH}" /share/CACHEDEV1_DATA/.qpkg/autorun/autorun.sh && \
-chmod +x /share/CACHEDEV1_DATA/.qpkg/autorun/autorun.sh
+rm -rf /share/CE_CACHEDEV1_DATA/.qpkg/autorun && \
+mkdir -p /share/CE_CACHEDEV1_DATA/.qpkg/autorun && \
+cp "${SCRIPTPATH}" /share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh && \
+chmod +x /share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh
 )
