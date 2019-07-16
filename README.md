1079f146dcf917ce107a14d57306cfe79f3e0bb2
Henning Häcker <henning.haecker@web.de>
2019-07-16 12:17:14 +0200

    final version

--- README.md
+++ README.md
@@ -3,8 +3,18 @@
 ## .profile
 `/root/.profile`
 
-## autorun.sh
-`/share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh`
+## /etc/config/autorun.sh aktivieren
+https://wiki.qnap.com/wiki/Running_Your_Own_Application_at_Startup
+Starting from QTS 4.3.3 it's required to enable the autorun.sh processing in the Control Panel -> Hardware -> General: Run user defined startup processes (autorun.sh)
+
+`cat /etc/config/autorun.sh`
+```
+#!/bin/sh
+/share/CE_CACHEDEV1_DATA/autorun/autorun.sh
+```
+
+## /share/CE_CACHEDEV1_DATA/autorun.sh
+Executable defined in `autorun.sh`
 
 ## qpkg.conf
 `/etc/config/qpkg.conf`
--- autorun.sh
+++ autorun.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-#This script needs to be executed on a QNAP NAS to install itself
-
-#full path to this bash script (autorun.sh)
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
 #fix security vulnerabilities
 rm -rf /usr/sbin/noip2
 rm -rf /usr/sbin/noip2c
 
-#add autorun if necessarry
-grep autorun /etc/config/qpkg.conf > /dev/null || \
-(
-echo "
-
-[autorun]
-Name = autorun
-Version = 1.0
-QPKG_File = autorun.qpkg // Ohne diese Zeile wird das Script deaktiviert!
-Shell = /share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh // ggf. anpassen
-Install_Path = /share/CE_CACHEDEV1_DATA/.qpkg/autorun // ggf. anpassen
-Enable = TRUE
-Official = 0
-" >> /etc/config/qpkg.conf && \
-rm -rf /share/CE_CACHEDEV1_DATA/.qpkg/autorun && \
-mkdir -p /share/CE_CACHEDEV1_DATA/.qpkg/autorun && \
-cp "${SCRIPTPATH}" /share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh && \
+#ensure autorun script is executable
 chmod +x /share/CE_CACHEDEV1_DATA/.qpkg/autorun/autorun.sh
-)
+
+#update dash profile
+rm -f /root/.profile
+cp /share/CE_CACHEDEV1_DATA/autorun/.profile /root/.profile
